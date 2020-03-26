b8bed64a43e4b52f034c6ec5d0598ce6bde31d33
yasmin <yjones@spartaglobal.com>
2020-03-26 15:47:51 +0000

    week one

---
+++ cat_Food.py
@@ -0,0 +1,11 @@
+number_of_cats = 10
+cans_eaten_daily = 2
+
+total_eaten = number_of_cats * cans_eaten_daily
+
+print(str(total_eaten) + " " + "cans eaten a day")
+
+total_eaten_in_week = (number_of_cats * cans_eaten_daily) * 7
+
+print(str(total_eaten_in_week) + " " + "total cans eaten in a week")
+
---
+++ weekOne.py
@@ -0,0 +1,27 @@
+print('hey my name is yasmin')
+
+print(5+5)
+
+print("cat" * 3)
+
+print("harry potter and the ".title())
+
+print("cat".upper())
+
+print("hey".lower())
+
+print( "cat" + str(3))
+
+username = "yasmin_1997"
+
+age = 23
+
+print(username)
+
+orange = 12
+cost_per_orange = 0.5
+
+total = orange + cost_per_orange
+
+print(str(orange) + " oranges")
+
